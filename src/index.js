1b6ca0e26c44ec0911875331ed317b2be7293d46
m51127x-code <m51127x@gmail.com>
2026-05-21 22:04:08 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "./index.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
